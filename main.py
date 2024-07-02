f496e218256b3a134fa3ed6de68ef86044b6448b
Ahmed <braika7med@gmail.com>
2024-07-02 20:30:51 +0100

    finish map drawing

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import numpy as np
 import pygame
 from pygame.locals import *
 
-CELLS = 60
-CELL_SIZE = 10
+CELLS = 10
+CELL_SIZE = 70
 SCREEN_SIZE = CELLS * CELL_SIZE
+
+START = 1
+START_COLOR = 'blue'
+TARGET = 2
+TARGET_COLOR = 'red'
+PATH = 3
+PATH_COLOR = 'green'
 
 class Game:
     def __init__(self) -> None:
@@ -25,13 +32,26 @@ class Game:
         pygame.quit()
 
     def draw_map(self) -> None:
+        # Draw cells in the map dependig on their value
         for i, row in enumerate(self.map):
             for j, cell in enumerate(row):
-                if cell == 0:
-                    color = 'black'
+                if cell == 0: # if cell is zero keep the background color
+                    continue
+                if cell == START:
+                    color = START_COLOR
+                elif cell == TARGET:
+                    color = TARGET_COLOR
+                elif cell == PATH:
+                    color = PATH_COLOR
                 pygame.draw.rect(self.screen,
                                  color,
                                  pygame.Rect(i*CELL_SIZE, j*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                
+        for i in range(CELL_SIZE, SCREEN_SIZE, CELL_SIZE): # Start from CELL SIZE to hide the two lines in top and left
+            # Draw horizantal lines
+            pygame.draw.line(self.screen, 'white', (0, i), (SCREEN_SIZE, i), 2)
+            #  Draw vertical lines
+            pygame.draw.line(self.screen, 'white', (i, 0), (i, SCREEN_SIZE), 2)
 
 if __name__ == '__main__':
     Game().run()
